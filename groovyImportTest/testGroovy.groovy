79be48e702c3df0415328a75ac67f072916d744b
gaulbaram0111 <gaulbaram0111@gmail.com>
2023-06-22 18:10:12 +0900

    230622

--- groovyImportTest/testGroovy.groovy
+++ groovyImportTest/testGroovy.groovy
@@ -1,6 +1,6 @@
-File sourceFile = new File("D:/workspace/emailTemplateStudy/groovyImportTest/importGroovy.groovy");
+File sourceFile = new File("importGroovy.groovy"); // 불러올파일 경로
 Class groovyClass = new GroovyClassLoader(getClass().getClassLoader()).parseClass(sourceFile);
 GroovyObject myObject = (GroovyObject) groovyClass.newInstance();
 
-
 println myObject.codeBook.get("bankCodes").get("000")
+
